f3de938c821b73906d0f6047371729924ad4a3b4
CoolPatoGuy5 <tighovanes@gmail.com>
2026-09-15 17:57:14 +0000

    update

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -5,5 +5,19 @@
 class LectureInput{
     public static void main(String args[]) {
         // Your Code Goes here!
+        System.out.println("🇲🇽 Welcome to Tigran's Mexican restaurant! 🇲🇽");
+        System.out.println("Menu:");
+
+        System.out.print("1. chile y queso tamales - ");
+        double item1 = 11.95;
+        System.out.println(item1);
+
+        System.out.print("2. choy riso tacos - ");
+        double item2 = 9.25;
+        System.out.println(item2);
+
+        System.out.print("3. horchata - ");
+        double item3 = 5.50;
+        System.out.println(item3);
 	}
 }
